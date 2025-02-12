ce26e65111e46d8a9de45734f2d4498ef113d486
Hochul Shin <bryson@Hochuls-MacBook-Pro.local>
2025-02-12 11:19:32 +0900

    add the models

--- src/main/java/com/example/DishOpenHour.java
+++ src/main/java/com/example/DishOpenHour.java
@@ -1,4 +1,20 @@
 package com.example;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import lombok.*;
+
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class DishOpenHour {
+    private Long id;
+    private Long dishId;
+    private String dayOfWeek;
+    private Integer fromHour;
+    private Integer fromMinute;
+    private Integer toHour;
+    private Integer toMinute;
 }
---
+++ src/main/java/com/example/DishOptionItem.java
@@ -0,0 +1,34 @@
+package com.example;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import lombok.*;
+
+import java.util.List;
+import java.util.Map;
+
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class DishOptionItem {
+    private Long id;
+    private Long optionId;
+    private Map<String, String> names;
+    private Boolean quantityChangeable;
+    private Integer maxQuantity;
+    private Integer minQuantity;
+    private String type;
+    private Double salePrice;
+    private String currencyType;
+    private String taxBaseType;
+    private String restrictionType;
+    private String saleFromAt;
+    private String saleToAt;
+    private String targetAvailableTime;
+    private Boolean deleted;
+    private Boolean disposable;
+    private Double disposablePrice;
+    private Double displayPrice;
+}
